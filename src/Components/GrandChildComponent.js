6f113a9ca715a7b86fbc9c620c818947fff14e5a
Animesh Raj <animesh080808@gmail.com>
2023-05-26 17:37:46 +0530

    Use Context Via Hooks
    Signed-off-by: Animesh Raj <animesh080808@gmail.com>

--- src/Components/GrandChildComponent.js
+++ src/Components/GrandChildComponent.js
@@ -1,7 +1,13 @@
+import { useContext } from 'react';
+import colorContext from '../../context.js';
+
 const GrandChildComponent = () => {
+    const value = useContext(colorContext);
+    console.log(value);
     return (
         <div>
             <h1>Grand Child Component</h1>
+            <h4>{value.color}</h4>
         </div>
     );
 };
